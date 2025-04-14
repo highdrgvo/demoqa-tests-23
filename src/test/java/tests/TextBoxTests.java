3981f0892a1baabd0b58c3e3236bdb2471010e05
sergeyplechko <sergeyplechko@gmail.com>
2025-04-14 21:38:24 +0300

    added textBoxTests (new java class)

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -14,6 +14,7 @@ public class TextBoxTests {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
 
     }
 
