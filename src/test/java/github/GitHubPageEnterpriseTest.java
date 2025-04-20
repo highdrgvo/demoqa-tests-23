fd34303e9f29cf179dd94ee6633aca649952c37a
sergeyplechko <sergeyplechko@gmail.com>
2025-04-20 17:17:58 +0300

    added hw, class - GitHubPageEnterpriseTest

---
+++ src/test/java/github/GitHubPageEnterpriseTest.java
@@ -0,0 +1,30 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+public class GitHubPageEnterpriseTest {
+
+    @Test
+
+    void gitHubCheckPageEnterpriseTest() {
+
+        open("https://github.com/");
+        Configuration.browserSize="1920x1080";
+
+        $(byText("Solutions")).hover();
+        $(byText("Enterprises")).click();
+
+        $("#hero-section-brand-heading").shouldBe(visible).shouldHave(Condition.text("The AI-powered developer platform"));
+
+
+        sleep(5000);
+
+    }
+}
